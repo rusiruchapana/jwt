9165b4d82bb295c6b971c2c0e8056ae7a245a1ff
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 22:09:41 +0530

    Update JWTService.java

--- jwt authorization/security/src/main/java/com/rusiruchapana/security/service/JWTService.java
+++ jwt authorization/security/src/main/java/com/rusiruchapana/security/service/JWTService.java
@@ -1,4 +1,7 @@
 package com.rusiruchapana.security.service;
 
 public class JWTService {
+
+    private static final String secret = "hQXxxYG1gFlfmTaQQLOnGjp6cTkfNI8UtNMc6Jo0nCO2v826bhBr23UFn7hyc35jfa54EksorFoBoAprHW/GIA==";
+
 }
