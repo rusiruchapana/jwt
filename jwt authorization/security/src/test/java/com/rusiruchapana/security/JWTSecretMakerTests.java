00a619934733dc2e1cd8395ec0f9d9a3e6b52331
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 22:06:40 +0530

    create generate secret key method.

--- jwt authorization/security/src/test/java/com/rusiruchapana/security/JWTSecretMakerTests.java
+++ jwt authorization/security/src/test/java/com/rusiruchapana/security/JWTSecretMakerTests.java
@@ -1,4 +1,18 @@
 package com.rusiruchapana.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 public class JWTSecretMakerTests {
+
+    @Test
+    public void generateSecretKey(){
+        SecretKey secretKey = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+        String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        System.out.println(encodedKey);
+    }
 }
