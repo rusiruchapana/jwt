d3ee878593a79acc115dd76fb6ba7e0767b3caa7
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-13 10:37:55 +0530

    Update JWTService.java

--- jwt authorization/security/src/main/java/com/rusiruchapana/security/service/JWTService.java
+++ jwt authorization/security/src/main/java/com/rusiruchapana/security/service/JWTService.java
@@ -1,17 +1,9 @@
 package com.rusiruchapana.security.service;
 
-import io.jsonwebtoken.Jwts;
-import org.springframework.security.core.userdetails.UserDetails;
-
 public class JWTService {
-
     private static final String secret = "hQXxxYG1gFlfmTaQQLOnGjp6cTkfNI8UtNMc6Jo0nCO2v826bhBr23UFn7hyc35jfa54EksorFoBoAprHW/GIA==";
 
-    public String generateToken(UserDetails userDetails){
 
-
-        return null;
-    }
 
 
 
